66440991d8653dfd7032072966f48d0a55effe03
xkimido <kimido987@gmail.com>
2023-11-06 17:51:22 +0900

    Post 등록 API 작성

---
+++ src/main/java/com/xkimido/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.xkimido.service.posts;
+
+import com.xkimido.domain.posts.PostsRepository;
+import com.xkimido.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}

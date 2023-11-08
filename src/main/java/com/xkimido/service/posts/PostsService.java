df23f24a11548a90ab06b8637e8cba8ced69b69d
xkimido <kimido987@gmail.com>
2023-11-09 00:49:28 +0900

    게시글 조회 완료

--- src/main/java/com/xkimido/service/posts/PostsService.java
+++ src/main/java/com/xkimido/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.xkimido.service.posts;
 
 import com.xkimido.domain.posts.Posts;
 import com.xkimido.domain.posts.PostsRepository;
+import com.xkimido.web.dto.PostsListResponseDto;
 import com.xkimido.web.dto.PostsResponseDto;
 import com.xkimido.web.dto.PostsSaveRequestDto;
 import com.xkimido.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +33,18 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new
                 IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/xkimido/web/IndexController.java
+++ src/main/java/com/xkimido/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.xkimido.web;
 
+import com.xkimido.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 

6ae9815a17bfb953c313871fa9897bda6f5190c9
xkimido <kimido987@gmail.com>
2023-11-14 21:43:03 +0900

    어노테이션 기반 리팩터링

--- src/main/java/com/xkimido/web/IndexController.java
+++ src/main/java/com/xkimido/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.xkimido.web;
 
+import com.xkimido.config.auth.LoginUser;
 import com.xkimido.config.auth.dto.SessionUser;
 import com.xkimido.service.posts.PostsService;
 import com.xkimido.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
